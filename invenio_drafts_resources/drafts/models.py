fca3fd5178260678a3270c579ac6a0f8db2f9cf8
Pablo Panero <pablo.panero@cern.ch>
2020-08-11 11:55:52 +0200

    model: simplify draft base by inhereiting from record base

--- invenio_drafts_resources/drafts/models.py
+++ invenio_drafts_resources/drafts/models.py
@@ -12,30 +12,16 @@ import uuid
 from datetime import datetime
 
 from invenio_db import db
-from invenio_records.models import Timestamp
+from invenio_records.models import RecordMetadataBase
 from sqlalchemy.dialects import mysql, postgresql
 from sqlalchemy_utils.types import JSONType, UUIDType
 
 
-class DraftMetadataBase(Timestamp):
-    """Represent a base class for draft metadata.
-
-    The DraftMetadata object  contains a `created` and  a `updated`
-    properties that are automatically updated.
-    """
-
-    id = db.Column(
-        UUIDType,
-        primary_key=True,
-        default=uuid.uuid4,
-    )
-    """Draft identifier, same as the record it drafts."""
+class DraftMetadataBase(RecordMetadataBase):
+    """Represent a base class for draft metadata."""
 
     fork_version_id = db.Column(db.Integer)
     """Version id of the record it is draft of."""
-
-    version_id = db.Column(db.Integer, nullable=False)
-    """Used by SQLAlchemy for optimistic concurrency control."""
 
     expiry_date = db.Column(
         db.DateTime().with_variant(mysql.DATETIME(fsp=6), "mysql"),
@@ -43,23 +29,3 @@ class DraftMetadataBase(Timestamp):
         nullable=True
     )
     """Specifies when the draft expires. If `NULL` the draft doesn't expire."""
-
-    json = db.Column(
-        db.JSON().with_variant(
-            postgresql.JSONB(none_as_null=True),
-            'postgresql',
-        ).with_variant(
-            JSONType(),
-            'sqlite',
-        ).with_variant(
-            JSONType(),
-            'mysql',
-        ),
-        default=lambda: dict(),
-        nullable=True
-    )
-    """Store metadata in JSON format.
-    When you create a new `Record the `json field value should never be
-    `NULL`. Default value is an empty dict. `NULL` value means that the
-    record metadata has been deleted.
-    """
