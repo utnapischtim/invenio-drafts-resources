b1dd3b60914c904f91334c327c46ec3dff8071f4
Pablo Panero <pablo.panero@cern.ch>
2020-11-02 08:44:53 +0100

    workflows: add tests and remove travis file

--- run-tests.sh
+++ run-tests.sh
@@ -7,8 +7,14 @@
 # Invenio-Drafts-Resources is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
 
-python -m check_manifest --ignore ".travis-*" && \
-python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+python -m check_manifest --ignore ".*-requirements.txt"
+python -m sphinx.cmd.build -qnNW docs docs/_build/html
 docker-services-cli up es postgresql
 python -m pytest
 tests_exit_code=$?
