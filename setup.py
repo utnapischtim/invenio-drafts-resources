3ccc7c21542545c0028770091156c383db82eee4
Guillaume Viger <fenekku@fenekku.com>
2021-01-28 17:09:07 -0600

    dependencies: cap records-resources

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup_requires = [
 
 install_requires = [
     "invenio-pidrelations>=0.1.0,<2.0.0",
-    "invenio-records-resources>=0.10.1",
+    "invenio-records-resources>=0.10.1,<0.11.0",
 ]
 
 packages = find_packages()
